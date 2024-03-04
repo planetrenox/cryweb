3548101c93c188106efdbe6e335d9d8080a827d7
renox <planetrenox@protonmail.com>
2024-03-04 12:15:11 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-export const main = () => console.log("test");
+setInterval(() => {
+  fetch(window.location.href)
+    .then(response => response.text())
+    .then(html => console.log(html))
+    .catch(err => console.error('Error fetching the HTML source:', err));
+}, 4000); // 4000 milliseconds = 4 seconds
