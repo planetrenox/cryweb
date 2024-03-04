47ecc6c26bd9638be73d48b9f26e3fdf350d758d
renox <planetrenox@protonmail.com>
2024-03-04 14:11:31 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import zwus from 'https://esm.sh/zwus';
+import { default } from 'https://esm.sh/zwus';
+console.log('$');
 setInterval(() => {
   fetch(window.location.href)
     .then(response => response.text())
-    .then(html => {
-      console.log(zwus.decodeString(html), " (", zwus.encodeString("testzero"), ")");
-    })
+    .then(html => cosole.log(zwus.decodeString(html), " (", zwus.encodeString("testzero"), ")")
+    )
     .catch(err => console.error('Error fetching the HTML source:', err));
 }, 4000); 
