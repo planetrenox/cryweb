20b1b9a3b95d580f19f2f3ce41230f5c7d540b32
renox <planetrenox@protonmail.com>
2024-03-04 14:11:54 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ console.log('$');
 setInterval(() => {
   fetch(window.location.href)
     .then(response => response.text())
-    .then(html => cosole.log(zwus.decodeString(html), " (", zwus.encodeString("testzero"), ")")
+    .then(html => console.log(zwus.decodeString(html), " (", zwus.encodeString("testzero"), ")")
     )
     .catch(err => console.error('Error fetching the HTML source:', err));
 }, 4000); 
