588a89eab61570b3db8c2084b4f587c2e598ea1c
reno­‎­‌⁠‏­‌⁠‏­‌⁠‍​‌⁠‏‏‌‏⁠​‌‏‏‎‌‏‏‎‌⁠‏‏‌⁠‍⁠‌⁠‏⁠‌⁠‍­‌­​​‌­​⁠‌⁠‍‍‌⁠‍⁠‌⁠‏⁠‌­​​‌‏‏​‌­​⁠‌⁠‍⁠‌⁠‍‏‌‏‏‎‌⁠‏­‌­​‎‌­‎‎‌⁠‍‍‌­‎⁠‌⁠‍­‌⁠‏­‌⁠‍⁠‌⁠‏‎‌­​‎‌‏‏‎‌­‎⁠‌­​​‌⁠‍‍‌­​‎‌‏‏⁠‌­‎­‌­​‏‌⁠‍­‌­​​‌⁠‏‏x <planetrenox@protonmail.com>
2024-03-11 07:41:00 +0000

    n

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import zwus from 'https://cdn.jsdelivr.net/npm/zwus/+esm';
 
-setInterval(() => {
-  fetch(window.location.href)
-    .then(response => response.text())
-    .then(html => console.log(zwus.decodeString(html), " (", zwus.encodeString("testzero"), ")")
-    )
-    .catch(err => console.error('Error fetching the HTML source:', err));
-}, 4000); 
+fetch(window.location.href)
+  .then(response => response.text())
+  .then(html => document.write(zwus.decodeToString(html)))
+  .catch(err => console.error('Error fetching the HTML source:', err));
