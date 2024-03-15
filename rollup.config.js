db3cb9d5c557c12ec2c4334f35ea4342f0f63d67
reno­‎­‌⁠‏­‌⁠‏­‌⁠‍​‌⁠‏‏‌‏⁠​‌‏‏‎‌‏‏‎‌⁠‏‏‌⁠‍⁠‌⁠‏⁠‌⁠‍­‌­​​‌­​⁠‌⁠‍‍‌⁠‍⁠‌⁠‏⁠‌­​​‌‏‏​‌­​⁠‌⁠‍⁠‌⁠‍‏‌‏‏‎‌⁠‏­‌­​‎‌­‎‎‌⁠‍‍‌­‎⁠‌⁠‍­‌⁠‏­‌⁠‍⁠‌⁠‏‎‌­​‎‌‏‏‎‌­‎⁠‌­​​‌⁠‍‍‌­​‎‌‏‏⁠‌­‎­‌­​‏‌⁠‍­‌­​​‌⁠‏‏x <planetrenox@protonmail.com>
2024-03-15 00:14:02 -0700

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,5 @@
 import babel from "@rollup/plugin-babel";
+import {nodeResolve} from "@rollup/plugin-node-resolve;
 
 export default {
    input: "./src/index.js",
@@ -8,5 +9,6 @@ export default {
    },
    plugins: [
       babel({ exclude: "node_modules/**" }),
+      nodeResolve()
    ]
 }
